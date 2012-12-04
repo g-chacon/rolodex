f97d988ff03f1e5997c222ca6ffc7d42cea40649
Gonzalo <gonzalo@ubuntu.(none)>
2012-12-04 07:16:14 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rolodex::Application.config.secret_token = 'bb18dc8758c987dce3ef2981d2cf74b8ee4502d4248a87a3f4736df04539894cf45b26592db82e596751585c5a7542262ace8bd62a7cd11f69006948a88901ce'
